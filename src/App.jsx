2643f13b45b03c452f36157b264e57f10704be97
LuizaMGama <luizamgama11@gmail.com>
2024-05-20 20:54:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Cronometro from "./componentes/Cronometro/Cronometro";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div id="cronometro-container">
+      <Cronometro />
+    </div>
+  );
+}
